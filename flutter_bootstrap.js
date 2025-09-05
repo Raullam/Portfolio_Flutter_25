c76140d23495cafee4b3c209313b64d4d71712c0
RaulRLM <raullama@paucasesnovescifp.com>
2025-09-05 08:50:21 +0200

    Mejoras en skills pagina detalle tamaplants y contacto los botones ahora no se salen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "563685158"
+    serviceWorkerVersion: "1252433721"
   }
 });
