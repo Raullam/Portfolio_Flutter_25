d3d41af227d39180086f77429beb9341ca93ce38
RaulRLM <raullama@paucasesnovescifp.com>
2025-09-10 11:18:33 +0200

    canvios en el boton concretar cita y inicio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1229031967"
+    serviceWorkerVersion: "1401261474"
   }
 });
